3c3f6139019aeb91e19f06b480867a574ad9a7b9
Benjamin Plaut <bplaut@unix2.andrew.cmu.edu>
2015-09-09 16:52:38 -0400

    Commented datatypesv1.ml. Also removed TODO file, will use fb group instead.

--- rattata/lab1/src/util/datatypesv1.ml
+++ rattata/lab1/src/util/datatypesv1.ml
@@ -1,17 +1,29 @@
+(* Will eventually add Pointer of pointer, etc *)
 type const = Int of int
+(* movl, addl, cdq, and so on *)
 type instrName = InstrName of string
 
 type reg = EAX | ECX | EBX | EIX | EBP | RSP | BILLYLOOKUPTHEOTHERS
 
+(* the int is the memory offest from the register *)
 type memAddr = reg * int
 
+(* These are for actual assembly instructions. Tmps are not allowed. *)
 type assemArg = Reg of reg | MemAddr of memAddr | Const of const
 type assemArgs = NONE | Arg of assemArg | SrcDest of assemArg * assemArg
 type assemInstr = instrName * assemArgs
 
+(* These allow tmps, but also allow actual assembly instructions.
+   These are used for all the steps that involve tmps.
+   Note: Because we allow actual assembly instructions, memory
+   addresses are allowed. Memory addresses should not occur prior
+   to register allocation, at least not in L1 (unless I'm missing
+   something) *)
 type tmp = int
 type tmpAssemArg = Tmp of tmp | AssemArg of assemArg
-type tmpAssemArgs = TmpNONE | TmpArg of tmpAssemArg | TmpSrcDest of tmpAssemArg * tmpAssemArg
+type tmpAssemArgs = TmpNONE | TmpArg of tmpAssemArg
+                  | TmpSrcDest of tmpAssemArg * tmpAssemArg
+                                  
 type tmpTwoAddrInstr = instrName * tmpAssemArgs
 
 
