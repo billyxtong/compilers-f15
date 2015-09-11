b56eb0bdb54a45ddc2109e89d36d1b40761719c2
Benjamin Plaut <bplaut@unix1.andrew.cmu.edu>
2015-09-11 11:04:33 -0400

    more datatypes

--- rattata/lab1/src/util/datatypesv1.ml
+++ rattata/lab1/src/util/datatypesv1.ml
@@ -1,5 +1,5 @@
 type c0type = INT | PTR (* will add more eventually *)
-type const = Int of int
+type const = int * c0type
 
 type reg = RAX | RBX | RCX | RDX | RBP | RSP | RSI | RDI | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
 
@@ -7,9 +7,10 @@ type reg = RAX | RBX | RCX | RDX | RBP | RSP | RSI | RDI | R8 | R9 | R10 | R11 |
 type memAddr = reg * int
 
 (* These are for actual assembly instructions. Tmps are not allowed. *)
-type assemArg = Reg of reg | MemAddr of memAddr | Const of const
-type assemBinop = ADD of assemArg * assemArg | MUL of assemArg * assemArg
-type assemInstr = MOV of assemArg * assemArg | BINOP of assemBinop | RETURN
+type assemLoc = Reg of reg | MemAddr of memAddr
+type assemArg = AssemLoc of assemLoc | Const of const
+type assemBinop = ADD of assemArg * assemLoc | MUL of assemArg * assemLoc
+type assemInstr = MOV of assemArg * assemLoc | BINOP of assemBinop | RETURN
 
 (* Below here allows tmps, but also allows actual assembly instructions.
    Note: Because we allow actual assembly instructions, memory
@@ -17,22 +18,22 @@ type assemInstr = MOV of assemArg * assemArg | BINOP of assemBinop | RETURN
    to register allocation, at least not in L1 (unless I'm missing
    something) *)
 type tmp = int
-type tmpAssemArg = Tmp of tmp | AssemArg of assemArg
+type tmpAssemLoc = Tmp of tmp | AssemLocForTmp of assemLoc
+type tmpAssemArg = TmpAssemLoc of tmpAssemLoc | AssemArg of assemArg
 
 (* For Two Address Code *)
-type tmp2AddrBinop = Tmp3AddrAdd of tmpAssemArg * tmpAssemArg
-                   | Tmp3AddrMul of tmpAssemArg * tmpAssemArg
-type tmp2AddrInstr = Tmp2AddrMov of tmpAssemArg * tmpAssemArg
+type tmp2AddrBinop = Tmp2AddrAdd of tmpAssemArg * tmpAssemLoc
+                   | Tmp2AddrMul of tmpAssemArg * tmpAssemLoc
+type tmp2AddrInstr = Tmp2AddrMov of tmpAssemArg * tmpAssemLoc
                    | Tmp2AddrBinop of tmp2AddrBinop
                    | Tmp2AddrReturn of tmpAssemArg
 
 (* For Three Address Code *)
-type tmp3AddrBinop = Tmp3AddrAdd of tmpAssemArg * tmpAssemArg
-                   | Tmp3AddrMul of tmpAssemArg * tmpAssemArg
-type tmp3AddrInstr = Tmp3AddrMov of tmpAssemArg * tmpAssemArg
+type tmp3AddrBinop = Tmp3AddrAdd of tmpAssemArg * tmpAssemArg *  tmpAssemLoc
+                   | Tmp3AddrMul of tmpAssemArg * tmpAssemArg *  tmpAssemLoc
+type tmp3AddrInstr = Tmp3AddrMov of tmpAssemArg * tmpAssemArg *  tmpAssemLoc
                    | Tmp3AddrBinop of tmp3AddrBinop
                    | Tmp3AddrReturn of tmpAssemArg
-
 (* TODO For Inf Address Code (any number of operands on right hand side *)
 
 (* Post-Elab AST
