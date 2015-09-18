24f0270c01afe9cba535c5873919fbcee68bb02a
William Tong <wxt@unix6.andrew.cmu.edu>
2015-09-18 16:58:59 -0400

    changes to printdatatypes.ml and .mli to reflect changes to datatypes

--- rattata/lab1/src/util/printDatatypes.ml
+++ rattata/lab1/src/util/printDatatypes.ml
@@ -6,7 +6,7 @@ let c0typeToString (c : c0type) =
         INT -> "int"
       | PTR -> "pointer"
 
-let constToString ((i, t) : const) = concat "" [string_of_int(i); " of type "; c0typeToString(t)]
+let constToString (c : const) = string_of_int(c)
 
 let regToString (r : reg) = 
   match r with
@@ -27,7 +27,9 @@ let regToString (r : reg) =
       | R14 -> "%r14"
       | R15 -> "%r15"
 
-let memAddrToString ((register, offset) : memAddr) = concat "" [string_of_int(offset); "("; regToString(register); ")"]
+let memAddrToString ((register, offset) : memAddr) = 
+    concat "" [string_of_int(offset); "("; 
+    regToString(register); ")"]
 
 let assemLocToString(loc : assemLoc) =
   match loc with
@@ -37,93 +39,128 @@ let assemLocToString(loc : assemLoc) =
 let assemArgToString (arg : assemArg) = 
   match arg with
         AssemLoc(loc) -> assemLocToString(loc)
-      | Const(value, _) -> concat "" ["$"; string_of_int(value)]
+      | Const(c) -> constToString(c)
 
 let assemBinopToString(binop : assemBinop) = 
   match binop with
-        ADD(src, dest) -> concat "" ["addl "; assemArgToString(src); ", "; assemLocToString(dest)]
-      | MUL(src, dest) -> concat "" ["mull "; assemArgToString(src); ", "; assemLocToString(dest)]
+        ADD(src, dest) -> 
+            concat "" ["addl "; 
+            assemArgToString(src); ", "; 
+            assemLocToString(dest)]
+      | SUB(src, dest) -> 
+            concat "" ["subl "; 
+            assemArgToString(src); ", "; 
+            assemLocToString(dest)]
+      | MUL(src, dest) -> 
+            concat "" ["imull "; 
+            assemArgToString(src); ", "; 
+            assemLocToString(dest)]
+      | FAKEDIV(src, dest) -> 
+            concat "" ["idivl "; 
+            assemArgToString(src); ", "; 
+            assemLocToString(dest)]
+      | FAKEMOD(src, dest) -> 
+            concat "" ["imodl "; 
+            assemArgToString(src); ", "; 
+            assemLocToString(dest)]
 
 let assemInstrToString(instr : assemInstr) = 
   match instr with
         MOV(src, dest) ->
-           concat "" ["movl "; assemArgToString(src); ", ";
-                      assemLocToString(dest)]
+            concat "" ["movl "; 
+            assemArgToString(src); ", ";
+            assemLocToString(dest)]
       | BINOP(binop) -> assemBinopToString(binop)
       | RETURN -> "ret"
-      | IDIV(divisor) ->
-           concat "" ["idiv "; assemArgToString(divisor)]
-      | CDQ -> "cdq"
 
 let assemProgToString(assemprog : assemProg) = 
-  concat "\n" (List.map assemInstrToString assemprog)
+  concat "\n" (List.map assemInstrToString assemprog) ^ "\n"
 
-let tmpToString(t : tmp) = concat "" ["t"; string_of_int t]
+let assemInstrWonkyToString(wonkyInstr : assemInstrWonky) = 
+  match wonkyInstr with
+        AssemInstr(instr) -> assemInstrToString(instr)
+      | CDQ -> "cdq"
+      | IDIV(divisor) ->
+           concat "" ["idiv "; 
+           assemArgToString(divisor)]
 
-let tmpAssemLocToString(tloc : tmpAssemLoc) = 
-  match tloc with
-        Tmp(t) -> tmpToString(t)
-      | AssemLocForTmp(aloc) -> assemLocToString(aloc)
+let assemProgWonkyToString(wonkyAssemProg : assemProgWonky) = 
+  concat "\n" (List.map assemInstrWonkyToString wonkyAssemProg) ^ "\n"
 
-let tmpAssemArgToString(tmpArg : tmpAssemArg) = 
-  match tmpArg with
-        TmpAssemLoc(t) -> tmpAssemLocToString(t)
-      | AssemArg(arg) -> (match arg with
-                                AssemLoc(loc) -> assemLocToString(loc)
-                              | Const(value, _) -> string_of_int(value))
+let tmpToString(Tmp(t) : tmp) = concat "" ["t"; string_of_int t]
+
+let tmpArgToString(tArg : tmpArg) = 
+  match tArg with
+        TmpLoc(t) -> tmpToString(t)
+      | TmpConst(c) -> constToString(c)
 
 let tmp2AddrBinopToString(tmp2binop : tmp2AddrBinop) =
   match tmp2binop with
-        Tmp2AddrAdd(tmpsrc, tmpdest) ->
-            concat "" [tmpAssemLocToString(tmpdest); " <-- "; 
-            tmpAssemLocToString(tmpdest); " + ";
-            tmpAssemArgToString(tmpsrc)]
-      | Tmp2AddrSub(tmpsrc, tmpdest) ->
-            concat "" [tmpAssemLocToString(tmpdest); " <-- ";
-            tmpAssemLocToString(tmpdest); " - ";
-            tmpAssemArgToString(tmpsrc)]
-      | Tmp2AddrMul(tmpsrc, tmpdest) -> concat "" [tmpAssemLocToString(tmpdest); " <-- "; 
-                                                    tmpAssemLocToString(tmpdest); " * "; tmpAssemArgToString(tmpsrc)]
-      | Tmp2AddrDiv(tmpsrc, tmpdest) ->
-            concat "" [tmpAssemLocToString(tmpdest); " <-- ";
-            tmpAssemLocToString(tmpdest); " / ";
-            tmpAssemArgToString(tmpsrc)]
-      | Tmp2AddrMod(tmpsrc, tmpdest) ->
-            concat "" [tmpAssemLocToString(tmpdest); " <-- ";
-            tmpAssemLocToString(tmpdest); " % ";
-            tmpAssemArgToString(tmpsrc)]
+        Tmp2AddrAdd(arg, temp) ->
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpToString(temp); " + ";
+            tmpArgToString(arg)]
+      | Tmp2AddrSub(arg, temp) ->
+            concat "" [tmpToString(temp); " <-- ";
+            tmpToString(temp); " - ";
+            tmpArgToString(arg)]
+      | Tmp2AddrMul(arg, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpToString(temp); " * "; 
+            tmpArgToString(arg)]
+      | Tmp2AddrDiv(arg, temp) ->
+            concat "" [tmpToString(temp); " <-- ";
+            tmpToString(temp); " / ";
+            tmpArgToString(arg)]
+      | Tmp2AddrMod(arg, temp) ->
+            concat "" [tmpToString(temp); " <-- ";
+            tmpToString(temp); " % ";
+            tmpArgToString(arg)]
 
 let tmp2AddrInstrToString(tmp2instr : tmp2AddrInstr) = 
   match tmp2instr with
-        Tmp2AddrMov(tmpsrc, tmpdest) -> concat "" [tmpAssemLocToString(tmpdest); " <-- "; tmpAssemArgToString(tmpsrc)]
+        Tmp2AddrMov(arg, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg)]
       | Tmp2AddrBinop(tmpbinop) -> tmp2AddrBinopToString(tmpbinop)
-      | Tmp2AddrReturn(tmparg) -> concat "" ["return "; tmpAssemArgToString(tmparg)]
+      | Tmp2AddrReturn(tmparg) -> 
+            concat "" ["return "; tmpArgToString(tmparg)]
 
 let tmp2AddrProgToString(tmp2addrprog : tmp2AddrProg) =
   (* We want a newline at the end *)
   concat "\n" (List.map tmp2AddrInstrToString tmp2addrprog) ^ "\n"
 
-let tmpWonkyInstrToString(wonkyinstr: tmpWonkyInstr) =
-   match wonkyinstr with
-      Tmp2AddrInstr(instr) -> tmp2AddrInstrToString instr
-    | TmpCDQ -> "cdq"
-    | TmpIDIV(tmpArg) -> concat "" ["idiv "; tmpAssemArgToString tmpArg]
-
-let tmpWonkyProgToString (wonkyprog: tmpWonkyProg) = 
-  concat "\n" (List.map tmpWonkyInstrToString wonkyprog) ^ "\n"
-
 let tmp3AddrBinopToString(tmp3binop : tmp3AddrBinop) =
   match tmp3binop with
-        Tmp3AddrAdd(tmpsrc1, tmpsrc2, tmpdest) -> concat "" [tmpAssemLocToString(tmpdest); " <-- "; 
-                                                    tmpAssemArgToString(tmpsrc1); " + "; tmpAssemArgToString(tmpsrc2)]
-      | Tmp3AddrMul(tmpsrc1, tmpsrc2, tmpdest) -> concat "" [tmpAssemLocToString(tmpdest); " <-- "; 
-                                                    tmpAssemArgToString(tmpsrc1); " * "; tmpAssemArgToString(tmpsrc2)]
+        Tmp3AddrAdd(arg1, arg2, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg1); " + "; 
+            tmpArgToString(arg2)]
+      | Tmp3AddrSub(arg1, arg2, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg1); " - "; 
+            tmpArgToString(arg2)]
+      | Tmp3AddrMul(arg1, arg2, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg1); " * "; 
+            tmpArgToString(arg2)]
+      | Tmp3AddrDiv(arg1, arg2, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg1); " / "; 
+            tmpArgToString(arg2)]
+      | Tmp3AddrMod(arg1, arg2, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg1); " % "; 
+            tmpArgToString(arg2)]
 
 let tmp3AddrInstrToString(tmp3instr : tmp3AddrInstr) = 
   match tmp3instr with
-        Tmp3AddrMov(tmpsrc, tmpdest) -> concat "" [tmpAssemLocToString(tmpdest); " <-- "; tmpAssemArgToString(tmpsrc)]
+        Tmp3AddrMov(arg, temp) -> 
+            concat "" [tmpToString(temp); " <-- "; 
+            tmpArgToString(arg)]
       | Tmp3AddrBinop(tmpbinop) -> tmp3AddrBinopToString(tmpbinop)
-      | Tmp3AddrReturn(tmparg) -> concat "" ["return "; tmpAssemArgToString(tmparg)]
+      | Tmp3AddrReturn(tmparg) -> 
+            concat "" ["return "; tmpArgToString(tmparg)]
 
 let tmp3AddrProgToString(tmp3addrprog : tmp3AddrProg) = 
-  concat "\n" (List.map tmp3AddrInstrToString tmp3addrprog)
+  concat "\n" (List.map tmp3AddrInstrToString tmp3addrprog) ^ "\n"
--- rattata/lab1/src/util/printDatatypes.mli
+++ rattata/lab1/src/util/printDatatypes.mli
@@ -8,15 +8,14 @@ val assemLocToString : assemLoc -> string
 val assemArgToString : assemArg -> string
 val assemBinopToString : assemBinop -> string
 val assemInstrToString : assemInstr -> string
-val assemProgToString : assemProg -> string (* add this to print *)
+val assemProgToString : assemProg -> string
+val assemInstrWonkyToString : assemInstrWonky -> string
+val assemProgWonkyToString : assemProgWonky -> string
 val tmpToString : tmp -> string
-val tmpAssemLocToString: tmpAssemLoc -> string
-val tmpAssemArgToString : tmpAssemArg -> string
+val tmpArgToString : tmpArg -> string
 val tmp2AddrBinopToString : tmp2AddrBinop -> string
 val tmp2AddrInstrToString : tmp2AddrInstr -> string
 val tmp2AddrProgToString : tmp2AddrProg -> string
-val tmpWonkyInstrToString : tmpWonkyInstr -> string
-val tmpWonkyProgToString : tmpWonkyProg -> string
 val tmp3AddrBinopToString : tmp3AddrBinop -> string
 val tmp3AddrInstrToString : tmp3AddrInstr -> string
 val tmp3AddrProgToString : tmp3AddrProg -> string
