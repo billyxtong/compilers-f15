918659ec1450127a30f11e479b56f3119a470b3b
William Tong <wxt@unix2.andrew.cmu.edu>
2015-09-29 23:02:30 -0400

    adding lab 2, this is so exciting

---
+++ rattata/lab2/compiler/back_end/formatAssem/formatAssem.ml
@@ -0,0 +1,7 @@
+
+(* Format the assemProgWonky into a string that can be written to
+   a .s file. Currently really simple. *)
+
+let formatAssem finalprog =
+    String.concat "" [".globl _c0_main\n"; "_c0_main:\n";
+                      PrintDatatypes.assemProgWonkyToString finalprog]
---
+++ rattata/lab2/compiler/util/symbol.mli
@@ -0,0 +1,26 @@
+(* C0 Compiler
+ * The symbol tables
+ * Author: Kaustuv Chaudhuri <kaustuv+@cs.cmu.edu>
+ * Converted to OCaml by Michael Duggan <md5i@cs.cmu.edu>
+ *)
+
+open Core.Std
+
+type t
+
+include Comparable.S with type t := t
+
+val compare : t -> t -> int (* compare symbols by their
+                                         creation time *)
+
+
+
+val bogus : t              (* a dummy symbol, less than others *)
+val is_bogus : t -> bool
+
+val reset : unit -> unit (* resets the hash table in which the
+                            symbols are stored *)
+
+val symbol : string -> t (* generates a new symbol with given name *)
+val name : t -> string     (* returns a name associated with symbol *)
+val format : Format.formatter -> t -> unit
