06c810e8a208f99f8f39172a8e0e970df38d62e9
Benjamin Plaut <bplaut@unix3.andrew.cmu.edu>
2015-10-02 15:28:59 -0400

    New datatypes for control flow; incomplete but enough to start working. Don't have all the bool ops set up in postElabAST for example.

--- rattata/lab2/compiler/util/datatypesv1.ml
+++ rattata/lab2/compiler/util/datatypesv1.ml
@@ -1,4 +1,4 @@
-type c0type = INT | BOOL (* will add more eventually *)
+type c0type = INT | BOOL
 (* everything in c0 is an int! *)              
 type const = int
 
@@ -8,21 +8,28 @@ type reg = EAX | EBX | ECX | EDX | RBP | RSP | ESI | EDI | R8 | R9 | R10 | R11 |
 type memAddr = reg * int
 
 (* These are for actual assembly instructions. Tmps are not allowed. *)
-type binop = ADD | MUL | SUB | FAKEDIV | FAKEMOD
+type intBinop = ADD | MUL | SUB | FAKEDIV | FAKEMOD
+              | BIT_AND | BIT_OR | BIT_XOR
+              | RSHIFT | LSHIFT              
 type assemLoc = Reg of reg | MemAddr of memAddr
 type assemArg = AssemLoc of assemLoc | Const of const
-type assemBinopInstr = binop * assemArg * assemLoc
-type jump = JNE | JE | JG | JGE | JLE | JL | JMP_UNCOND 
-type label = Label of int
+type boolInstr = LOG_AND of assemArg * assemLoc
+            | LOG_NOT of assemLoc
+            | TEST of assemArg * assemLoc
+            | CMP of assemArg * assemLoc
+type assemIntInstr = intBinop * assemArg * assemLoc
+type jump = JNE | JE | JG | JMP_UNCOND 
+type label = int
 type jumpInstr = jump * label
 type assemInstr = MOV of assemArg * assemLoc
                 | MOVQ of assemArg * assemLoc
-                | BINOP of assemBinopInstr
+                | INT_BINOP of assemIntInstr
                 | PUSH of reg
                 | POP of reg
                 | RETURN
-                | CMPL of assemArg * assemArg
-                | JMP of jumpInstr
+                | JUMP of jumpInstr
+                | BOOL_INSTR of boolInstr
+                | LABEL of label
 type assemProg = assemInstr list
 
 (* Assembly Code with wonky instructions (i.e. idiv, etc) *)
@@ -41,29 +48,45 @@ type assemProgWonky = assemInstrWonky list
    something) *)
 type tmp = Tmp of int
 type tmpArg = TmpLoc of tmp | TmpConst of const
-type tmpBinop = TmpBinop of binop
-type tmpJump = TmpJump of jump
 (* Two Address Code *)
-type tmp2AddrBinop = tmpBinop * tmpArg * tmp
-type tmp2AddrJump = tmpJump * label
+type tmpBoolInstr = TmpLogAnd of tmpArg * tmp
+            | TmpLogNot of tmp
+            | TmpTest of tmpArg * tmp
+            | TmpCmp of tmpArg * tmp
+type tmp2AddrBinop = intBinop * tmpArg * tmp
 type tmp2AddrInstr = Tmp2AddrMov of tmpArg * tmp
                    | Tmp2AddrBinop of tmp2AddrBinop
                    | Tmp2AddrReturn of tmpArg
-                   | Tmp2AddrJump of tmp2AddrJump
-                   | Tmp2AddrCompare of tmpArg * tmpArg
+                   | Tmp2AddrJump of jumpInstr
+                   | Tmp2AddrBoolInstr of tmpBoolInstr
+                   | Tmp2AddrLabel of label
 type tmp2AddrProg = tmp2AddrInstr list
 
 (* Three Address Code *)
-type tmp3AddrBinop = tmpBinop * tmpArg * tmpArg *  tmp
+type tmp3AddrBinop = intBinop * tmpArg * tmpArg *  tmp
 type tmp3AddrInstr = Tmp3AddrMov of tmpArg *  tmp
                    | Tmp3AddrBinop of tmp3AddrBinop
                    | Tmp3AddrReturn of tmpArg
+                   | Tmp3AddrJump of jumpInstr
+                   | Tmp3AddrBoolInstr of tmpBoolInstr
+                   | Tmp3AddrLabel of label
 type tmp3AddrProg = tmp3AddrInstr list
 
 (* Inf Address Code (any number of operands on right hand side *)
-type tmpExpr = TmpAssemArg of tmpArg
+type tmpExpr = TmpIntArg of tmpArg
              | TmpInfAddrBinop of tmpInfAddrBinop
-and tmpInfAddrBinop = tmpBinop * tmpExpr * tmpExpr *  tmp
+and tmpInfAddrBinop = intBinop * tmpExpr * tmpExpr *  tmp
+type tmpBoolExpr = TmpBoolArg of tmpArg
+                  | TmpInfAddrBoolInstr of tmpInfAddrBoolInstr
+and tmpInfAddrBoolInstr = TmpLogAnd of tmpBoolExpr * tmpExpr * tmp
+            | TmpLogNot of tmpExpr
+            | TmpTest of tmpArg * tmp
+            | TmpCmp of tmpArg * tmp
+
 type tmpInfAddrInstr = TmpInfAddrMov of tmpExpr * tmpExpr *  tmp
-                    | TmpInfAddrReturn of tmpExpr   
+                   | Tmp3AddrBinop of tmp3AddrBinop
+                   | Tmp3AddrJump of jumpInstr
+                   | Tmp3AddrBoolInstr of tmpBoolExpr
+                   | Tmp3AddrLabel of label
+                   | TmpInfAddrReturn of tmpExpr   
 type tmpInfAddrProg = tmpInfAddrInstr list
