f650a1bdc87be6d8ccb9751ede0f60e8f27350c8
Benjamin Plaut <bplaut@unix4.andrew.cmu.edu>
2015-10-03 14:46:45 -0400

    copied over changes to ast.mli, still don't know why this is necessary and kind of annoys me

---
+++ rattata/lab2/compiler/front_end/parse/ast.mli
@@ -0,0 +1,59 @@
+(* L1 Compiler
+ * Abstract Syntax Trees
+ * Author: Alex Vaynberg
+ * Modified: Frank Pfenning <fp@cs.cmu.edu>
+ *
+ * Modified: Anand Subramanian <asubrama@andrew.cmu.edu> Fall 2010
+ * Converted to OCaml by Michael Duggan <md5i@cs.cmu.edu>
+ *
+ * Forward compatible fragment of C0
+ *)
+
+open Datatypesv1
+
+(* Post-Elab AST
+   A restriced grammar from the Pre-Elab AST. See the elaboration
+   file (which I have not yet written) for more info. *)
+type ident = string
+type intExpr = IntConst of const | IntIdent of ident
+          | ASTBinop of intExpr * intBinop * intExpr
+and boolExpr = BoolConst of const | BoolIdent of ident
+           | GreaterThan of intExpr * intExpr
+           | IntEquals of intExpr * intExpr
+           | BoolEquals of boolExpr * boolExpr
+           | LogNot of boolExpr
+           | LogAnd of boolExpr * boolExpr
+and expr = IntExpr of intExpr | BoolExpr of boolExpr               
+and assignStmt = ident * expr 
+type stmt = Decl of ident * c0type * postElabAST
+          | AssignStmt of assignStmt
+          | If of boolExpr * postElabAST * postElabAST
+          | While of boolExpr * postElabAST
+          | Nop
+          | Return of intExpr
+and postElabAST = stmt list
+    
+(* Pre-Elab AST
+   Unfortunately, we have to wrap everything in different
+   constructors here, in order to keep in separate from
+   Post-Elab AST *)
+type preElabExpr = PreElabConstExpr of const
+                 | IdentExpr of ident
+                 | PreElabBinop of preElabExpr * intBinop * preElabExpr
+type preElabDecl = NewVar of ident * c0type
+                 | Init of (ident * c0type * preElabExpr)
+type simpStmt = PreElabDecl of preElabDecl                        
+              | SimpAssign of ident * assignOp * preElabExpr
+              | SimpStmtExpr of preElabExpr
+type simpOpt = EmptySimp | HasSimpStmt of simpStmt
+type elseOpt = EmptyElse | PreElabElse of preElabStmt
+and control = PreElabIf of preElabExpr * preElabStmt * elseOpt
+             | PreElabWhile of preElabExpr * preElabStmt
+             | PreElabFor of simpOpt * preElabExpr * simpOpt *
+                             preElabStmt
+             | PreElabReturn of preElabExpr
+and preElabStmt = SimpStmt of simpStmt
+                 | Control of control
+                 | Block of block
+and block = preElabStmt list                      
+type preElabAST = block
