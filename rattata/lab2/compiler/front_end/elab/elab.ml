0b9e0e58337e2288a43d37ed63e74a6cc5f51e40
billyxtong <wxt@unix.andrew.cmu.edu>
2015-10-03 22:52:13 -0400

    did some elab stuff

--- rattata/lab2/compiler/front_end/elab/elab.ml
+++ rattata/lab2/compiler/front_end/elab/elab.ml
@@ -1,1 +1,28 @@
-(* hello *)
+open Ast
+
+let elaboratePreElabStmt (statement : preElabStmt) =
+  match statement with
+        SimpStmt(s) -> elaborateSimpStmt(s)
+      | Control(c) -> elaborateControl(c)
+      | Block(b) -> elaborateBlock(b)
+
+and elaborateSimpStmt (simpleStatement : simpStmt) =
+  match simpleStatement with
+        PreElabDecl(p) -> ""
+      | SimpAssign(i, p) -> ""
+      | SimpStmtExpr(p) -> ""
+
+and elaborateControl (ctrl : control) =
+  match ctrl with
+        PreElabIf(pExpr, pStmt, eOpt) -> ""
+      | PreElabWhile(pExpr, pStmt) -> ""
+      | PreElabFor(sOpt,pExpr,sOpt,pStmt) -> ""
+      | PreElabReturn(pExpr) -> ""
+
+and elaborateBlock (blk : block) = elaborateAST blk
+
+
+let elaborateAST (statements : preElabAST) =
+  match statements with
+        [] -> []
+      | statement :: statements' -> elaboratePreElabStmt(statement) :: elaborateAST(statements')
