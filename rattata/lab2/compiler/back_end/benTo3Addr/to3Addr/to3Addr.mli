398eda4e638a25360614adcf70d2c2e204d375cb
Benjamin Plaut <bplaut@unix5.andrew.cmu.edu>
2015-10-05 10:09:28 -0400

    removed some old to3Addr unused files

--- rattata/lab2/compiler/back_end/benTo3Addr/to3Addr/to3Addr.mli
+++
@@ -1,7 +0,0 @@
-(* This step takes InfAddr code to 3Addr code. Currently, I'm taking
-   the starter code's original InfAddr datatype as in put.
-   Eventually, I will convert everything to use our InfAddr datatype,
-   and then I will write another function that takes that as input
-   (which is what we'll actually use). *)
-
-val to3Addr: Tree.stm list -> Datatypesv1.tmp3AddrProg
