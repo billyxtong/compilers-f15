d2e1aeecd9a8c915ecab99696a567447bc80969a
Benjamin Plaut <bplaut@unix3.andrew.cmu.edu>
2015-10-08 21:12:36 -0400

    removing mov instructions with same src and dest now, this can happen after register allocation

--- rattata/lab2/compiler/back_end/condenseMoves/condenseMoves.ml
+++ rattata/lab2/compiler/back_end/condenseMoves/condenseMoves.ml
@@ -1,43 +1,14 @@
-(* Removes unnecessary chains of moves, such as
-   movl a b
-   movl b c
-   movl c a
-   which might not be ok if the next line where addl b a or something,
-   where the fact that we moved a to b matters. We'll see I guess :P *)
 open Datatypesv1
 
-let rec handleMove curr_instrs start instrs (src, dest) =
-    let newStartFromSrc = (match src with
-                                Const _ -> None
-                              | AssemLoc loc -> Some loc) in
-    (* It can be the new start as long as it's not a constant *)
-    match start with
-        None -> condenseMovesRec (AssemInstr (MOV (src,dest))::curr_instrs)
-                  newStartFromSrc instrs
-      | Some startLoc -> (match src with
-            (* if it's a constant, it breaks the chain *)
-             Const _ -> curr_instrs @ [AssemInstr (MOV(src, dest))] @
-                        condenseMovesRec [] None instrs
-           | AssemLoc srcLoc ->
-                (if startLoc = dest then (* Chain ends, and remove it *)
-                condenseMovesRec [] None instrs (* Otherwise, chain continues *)
-                else condenseMovesRec (curr_instrs @
-                    AssemInstr (MOV(src,dest))::[]) start
-                     instrs))
+let rec removeNullMoves = function
+    [] -> []
+  | instr::instrs -> (match instr with
+                        (AssemInstr (MOV (src, dest))) ->
+                            (if src = AssemLoc dest then removeNullMoves instrs
+                             else instr::removeNullMoves instrs)
+                     |  _ -> instr::removeNullMoves instrs)
 
-and condenseMovesRec curr_instrs start = function
-     [] -> []
-   | (AssemInstr (MOV (src, dest))::instrs) ->
-     handleMove curr_instrs start instrs (src, dest)
-   | instr::instrs -> (* anything else breaks the chain *)
-         curr_instrs @ [instr] @ condenseMovesRec [] None instrs
-
-(* (\* curr_instrs is the current run of moves we're on *\) *)
-(* let condenseMoves instrs = condenseMovesRec [] None instrs *)
-
-open PrintDatatypes
-
-let rec condenseMoves = function
+let rec condenseMovesDoubles = function
      [] -> []
    | instr::[] -> instr::[]
    | instr1::instr2::instrs ->
@@ -45,6 +16,8 @@ let rec condenseMoves = function
              (AssemInstr (MOV (src1, dest1)),
               AssemInstr (MOV (src2, dest2))) ->
      if ((AssemLoc dest1) = src2) && ((AssemLoc dest2) = src1)
-     then instr1 :: condenseMoves instrs
-                   else instr1 :: condenseMoves(instr2::instrs)
-           | _ -> instr1 :: condenseMoves(instr2::instrs)
+     then instr1 :: condenseMovesDoubles instrs
+                   else instr1 :: condenseMovesDoubles(instr2::instrs)
+           | _ -> instr1 :: condenseMovesDoubles(instr2::instrs)
+
+let condenseMoves instrs = condenseMovesDoubles(removeNullMoves instrs)
