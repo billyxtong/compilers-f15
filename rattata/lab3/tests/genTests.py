132027256c20e5638bd97aa4ffe1c2562ad52d09
Benjamin Plaut <bplaut@unix1.andrew.cmu.edu>
2015-10-10 13:48:16 -0400

    started tests

---
+++ rattata/lab3/tests/genTests.py
@@ -0,0 +1,33 @@
+header = "//test return 0\n\n"
+num = 100
+args1 = ['int bloo' + str(i) for i in xrange(num)]
+args1str = ", ".join(args1)
+args2 = ['int blah' + str(i) for i in xrange(num)]
+args2str = ", ".join(args2)
+args1 = "(" + args1str + ")"
+args2 = "(" + args2str + ")"
+start = '5000'
+zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
+main = "int main () {\n\treturn blooblah(" + zeros + ");\n}\n\n"
+zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
+argsForBloo = ['bloo0 - 1' if i == 0 else 'bloo'+str(i) for i in xrange(num)]
+argsForBloo = ", ".join(argsForBloo)
+argsForBlah = ['blah0 - 1' if i == 0 else 'blah'+str(i) for i in xrange(num)]
+argsForBlah = ", ".join(argsForBlah)
+
+bloodef = 'int blooblah ' + args1 + " {\n\t"
+bloobody = 'if (bloo0 == 0) return bloo0; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
+blahdef = 'int blahbloo ' + args2 + " {\n\t"
+blahbody = 'if (blah0 == 0) return blah0; else return blooblah(' +argsForBlah + ");\n}\n\n"
+
+decl1 = "int blooblah" + args1 + ";\n"
+decl2 = "int blahbloo" + args2 + ";\n\n"
+with open("tests/test.l3", 'w') as f:
+    f.write(header)
+    f.write(decl1)
+    f.write(decl2)
+    f.write(main)
+    f.write(bloodef)
+    f.write(bloobody)
+    f.write(blahdef)
+    f.write(blahbody)
--- rattata/lab3/tests/regAllocKiller.py
+++ rattata/lab3/tests/regAllocKiller.py
@@ -1,33 +1,32 @@
-header = "//test return 0\n\n"
-num = 100
+header = "//test return 666\n//Where is your god now?\n\n"
+num = 5000
 args1 = ['int bloo' + str(i) for i in xrange(num)]
 args1str = ", ".join(args1)
 args2 = ['int blah' + str(i) for i in xrange(num)]
 args2str = ", ".join(args2)
 args1 = "(" + args1str + ")"
 args2 = "(" + args2str + ")"
-start = '5000'
-zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
+start = '666'
+zeros = ", ".join([('666' if i > 0 else start) for i in xrange(num)])
 main = "int main () {\n\treturn blooblah(" + zeros + ");\n}\n\n"
 zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
-argsForBloo = ['bloo0 - 1' if i == 0 else '0' for i in xrange(num)]
+argsForBloo = ['bloo0 - 1' if i == 0 else 'bloo'+str(i) for i in xrange(num)]
 argsForBloo = ", ".join(argsForBloo)
-argsForBlah = ['blah0 - 1' if i == 0 else '0' for i in xrange(num)]
+argsForBlah = ['blah0 - 1' if i == 0 else 'blah'+str(i) for i in xrange(num)]
 argsForBlah = ", ".join(argsForBlah)
 
 bloodef = 'int blooblah ' + args1 + " {\n\t"
-bloobody = 'if (bloo0 == 0) return bloo1; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
+bloobody = "".join(['\t666;\n' for i in xrange(666)])
+bloobody += 'return bloo666;\n}'
+# bloobody = 'if (bloo0 == 0) return bloo0; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
 blahdef = 'int blahbloo ' + args2 + " {\n\t"
-blahbody = 'if (blah0 == 0) return blah1; else return blooblah(' +argsForBlah + ");\n}\n\n"
+blahbody = 'if (blah0 == 0) return blah0; else return blooblah(' +argsForBlah + ");\n}\n\n"
 
 decl1 = "int blooblah" + args1 + ";\n"
 decl2 = "int blahbloo" + args2 + ";\n\n"
 with open("tests/test.l3", 'w') as f:
     f.write(header)
     f.write(decl1)
-    f.write(decl2)
     f.write(main)
     f.write(bloodef)
     f.write(bloobody)
-    f.write(blahdef)
-    f.write(blahbody)
