e0caedfb0176660f5de56ea39d2ed4fb78a79898
Benjamin Plaut <bplaut@unix1.andrew.cmu.edu>
2015-10-10 14:00:07 -0400

    ok done with tests for now, going to ballroom

--- rattata/lab3/tests/regAllocKiller.py
+++ rattata/lab3/tests/regAllocKiller.py
@@ -1,4 +1,4 @@
-header = "//test return 666\n//Where is your god now?\n\n"
+header = "//test return 666\n\n"
 num = 666
 args1 = ['int bloo' + str(i+1) for i in xrange(num)]
 args1str = ", ".join(args1)
@@ -15,10 +15,10 @@ argsForBloo = ", ".join(argsForBloo)
 argsForBlah = ['blah0 - 1' if i == 0 else 'blah'+str(i) for i in xrange(num)]
 argsForBlah = ", ".join(argsForBlah)
 
-bloodef = 'int blooblah ' + args1 + " {\n\t"
+bloodef = 'int blooblah ' + args1 + " {\n"
 bloobody = "".join(['\t666;\n' for i in xrange(666)])
 bloobody += "\t" + " + ". join(['bloo' + str(i+1) for i in xrange(num)])+ ";\n"
-bloobody += '\treturn 666;\n}'
+bloobody += '\treturn 666;\n}\n\n'
 # bloobody = 'if (bloo0 == 0) return bloo0; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
 blahdef = 'int blahbloo ' + args2 + " {\n\t"
 blahbody = 'if (blah0 == 0) return blah0; else return blooblah(' +argsForBlah + ");\n}\n\n"
@@ -27,7 +27,6 @@ decl1 = "int blooblah" + args1 + ";\n"
 decl2 = "int blahbloo" + args2 + ";\n\n"
 with open("test.l3", 'w') as f:
     f.write(header)
-    f.write(decl1)
-    f.write(main)
     f.write(bloodef)
     f.write(bloobody)
+    f.write(main)
