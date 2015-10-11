627cdad305ed21c8fc4ba98a7196f8e0207646b2
Benjamin Plaut <bplaut@unix4.andrew.cmu.edu>
2015-10-11 17:09:34 -0400

    finished tests I think, although there are some still some autograder errors, so will need to revisit that

--- rattata/lab3/tests/someHardTests/mutual-recursion.py
+++ rattata/lab3/tests/someHardTests/mutual-recursion.py
@@ -1,13 +1,15 @@
-header = "//test return 666\n\n"
-num = 666
-args1 = ['int bloo' + str(i+1) for i in xrange(num)]
+# two mutually recursive functions with lots of args but only one is
+# ever used
+header = "//test return 0\n\n"
+num = 100
+args1 = ['int bloo' + str(i) for i in xrange(num)]
 args1str = ", ".join(args1)
 args2 = ['int blah' + str(i) for i in xrange(num)]
 args2str = ", ".join(args2)
 args1 = "(" + args1str + ")"
 args2 = "(" + args2str + ")"
-start = '666'
-zeros = ", ".join([('666' if i > 0 else start) for i in xrange(num)])
+start = '5000'
+zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
 main = "int main () {\n\treturn blooblah(" + zeros + ");\n}\n\n"
 zeros = ", ".join([('0' if i > 0 else start) for i in xrange(num)])
 argsForBloo = ['bloo0 - 1' if i == 0 else 'bloo'+str(i) for i in xrange(num)]
@@ -15,18 +17,19 @@ argsForBloo = ", ".join(argsForBloo)
 argsForBlah = ['blah0 - 1' if i == 0 else 'blah'+str(i) for i in xrange(num)]
 argsForBlah = ", ".join(argsForBlah)
 
-bloodef = 'int blooblah ' + args1 + " {\n"
-bloobody = "".join(['\t666;\n' for i in xrange(666)])
-bloobody += "\t" + " + ". join(['bloo' + str(i+1) for i in xrange(num)])+ ";\n"
-bloobody += '\treturn 666;\n}\n\n'
-# bloobody = 'if (bloo0 == 0) return bloo0; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
+bloodef = 'int blooblah ' + args1 + " {\n\t"
+bloobody = 'if (bloo0 == 0) return bloo0; else return blahbloo(' +argsForBloo + ");\n}\n\n" 
 blahdef = 'int blahbloo ' + args2 + " {\n\t"
 blahbody = 'if (blah0 == 0) return blah0; else return blooblah(' +argsForBlah + ");\n}\n\n"
 
 decl1 = "int blooblah" + args1 + ";\n"
 decl2 = "int blahbloo" + args2 + ";\n\n"
-with open("test.l3", 'w') as f:
+with open("tests/test.l3", 'w') as f:
     f.write(header)
+    f.write(decl1)
+    f.write(decl2)
+    f.write(main)
     f.write(bloodef)
     f.write(bloobody)
-    f.write(main)
+    f.write(blahdef)
+    f.write(blahbody)
