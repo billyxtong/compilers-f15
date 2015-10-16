2e6714759af198293e600cd9ab171452b7b5433d
Benjamin Plaut <bplaut@unix3.andrew.cmu.edu>
2015-10-16 19:51:44 -0400

    datatypes for assem, I think...here we go on regAlloc...

--- rattata/lab3/compiler/util/datatypesv1.ml
+++ rattata/lab3/compiler/util/datatypesv1.ml
@@ -34,7 +34,9 @@ type assemInstr = MOV of assemArg * assemLoc
                 | BOOL_INSTR of boolInstr
                 | LABEL of label
                 | CALL of ident
-type assemProg = assemInstr list
+type assemFunDef = AssemFunDef of ident * assemLoc list *
+                                        assemInstr list
+type assemProg = assemFunDef list
 
 (* Assembly Code with wonky instructions (i.e. idiv, etc) *)
 (* This comes after 2Addr in the pipeline, but needs to be below
