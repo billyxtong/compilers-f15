df686d5e966718fa970c5d4553c02bd0d3e1fbda
Benjamin Plaut <bplaut@unix4.andrew.cmu.edu>
2015-10-17 12:54:09 -0400

    now regAlloc should (theoretically) work for more than 6 args, but still haven't tested at all. Also have separate lists for caller and callee saved regs now

--- rattata/lab3/compiler/back_end/regAlloc/allocForFun.ml
+++ rattata/lab3/compiler/back_end/regAlloc/allocForFun.ml
@@ -6,14 +6,21 @@ open Graph
 
 let spillReg = Reg EDI
 
-let pushPopList = [EBX; ESI; R8; R9; R10; R11; R12; R13; R14; R15; ECX; EDI]
+let bytesForArg = 8
+
+let calleeSavedList = [RBP; EBX; R12; R13; R14; R15]
+let callerSavedList = [ECX; EDI; ESI; R8; R9; R10; R11]
 
 let listToString i a = String.concat "" (List.map
             (fun x -> string_of_int(i)^": " ^string_of_int(x) ^ ", ") a @["\n"])
 
-let pushInstrs = List.map (fun r -> PUSH r) pushPopList
+let pushCalleeInstrs = List.map (fun r -> PUSH r) calleeSavedList
 
-let popInstrs = List.map (fun r -> POP r) (List.rev pushPopList)
+let popCalleeInstrs = List.map (fun r -> POP r) (List.rev calleeSavedList)
+
+let pushCallerInstrs = List.map (fun r -> PUSH r) callerSavedList
+
+let popCallerInstrs = List.map (fun r -> POP r) (List.rev callerSavedList)
 
 let assemLocForColor regArray offsetIncr colorNum =
     if (colorNum < A.length regArray) then Reg(A.get regArray colorNum)
@@ -44,11 +51,31 @@ let translateTmpArg tbl = function
                          but it was ok because the line was unreachable *)
                  with Not_found -> AssemLoc (MemAddr(RSP, -666)))
 
+let getArgDest paramRegArray i =
+    (* First 6 args go into paramRegArray. 7th arg goes 8 above RSP,
+       8th arg goes 16 above RSP, etc *)
+    if i < Array.length paramRegArray then paramRegArray.(i) else
+    (* This will the correct offset AFTER WE DECREASE RSP *)
+    MemAddr(RSP, bytesForArg * (i - Array.length paramRegArray + 1))
+
 let translateFunCall tbl finalOffset paramRegArray fName args dest =
-    (* First, move args into the expected registers. The ith arg does into the
-       ith element of paramRegArray *)
+    (* 1. Figure out how many args we'll need to put on the stack.
+       2. Figure out how much we'll need to decrease RSP by, both to make
+       room for the stack args, and to make sure it's 16-byte aligned.
+       3. Decrease RSP by the corresponding amount.
+       4. Move args into expected regs/memAddr
+       5. Call function
+       6. Restore RSP
+       7. Move result from EAX to wherever we want (if there is a result)
+    *)
+    let numStackArgs = List.length args - (Array.length paramRegArray) in
+    let spaceForArgs = bytesForArg * numStackArgs in
+    let is16ByteAligned = (finalOffset + spaceForArgs) mod 16 = 8 in
+    let rspShiftAmount = (if is16ByteAligned then spaceForArgs
+                          else spaceForArgs + 8) in
+            (* Adding 8 makes it 16-byte-aligned if it isn't *)
     let moveInstrs = List.mapi (fun i -> fun arg ->
-        MOV(translateTmpArg tbl arg, paramRegArray.(i))) args in
+        MOV(translateTmpArg tbl arg, getArgDest paramRegArray i)) args in
     (* See if we need to move EAX to a certain result tmp (we wouldn't have to
        for void function calls *)
     let resultInstr = (match dest with
@@ -56,9 +83,9 @@ let translateFunCall tbl finalOffset paramRegArray fName args dest =
                         | Some t -> MOV(AssemLoc (Reg EAX), H.find tbl t)::[]) in
     (* Now make sure we align RSP to 16 bytes. Note that we know that RSP
        is 8-byte-but-not-16-bye aligned at the moment. *)
-    let rspShiftAmount = 16 - ((finalOffset + 8) mod 16) in
-    moveInstrs @ [SUBQ(Const rspShiftAmount, Reg RSP)] @ [CALL fName]
-    @ [ADDQ(Const rspShiftAmount, Reg RSP)] @ resultInstr
+    pushCallerInstrs @
+    SUBQ(Const rspShiftAmount, Reg RSP):: moveInstrs @ [CALL fName]
+    @ [ADDQ(Const rspShiftAmount, Reg RSP)] @ popCallerInstrs @ resultInstr 
 
 let translate tbl finalOffset paramRegArray (instr : tmp2AddrInstr) : assemInstr list =
    match instr with
@@ -66,7 +93,8 @@ let translate tbl finalOffset paramRegArray (instr : tmp2AddrInstr) : assemInstr
       | Tmp2AddrBinop(binop, arg, dest) ->
               INT_BINOP(binop, translateTmpArg tbl arg, H.find tbl dest)::[]
       | Tmp2AddrReturn(arg) -> MOV(translateTmpArg tbl arg, Reg EAX)::
-                               ADDQ(Const finalOffset, Reg RSP)::[] @ (popInstrs)@ RETURN::[]
+                               ADDQ(Const finalOffset, Reg RSP)::[]
+                               @ (popCalleeInstrs)@ RETURN::[]
       | Tmp2AddrJump j -> JUMP j::[]
       | Tmp2AddrLabel jumpLabel -> LABEL jumpLabel::[]
       | Tmp2AddrBoolInstr TmpTest(arg, t) ->
@@ -99,15 +127,22 @@ let getTempList instrList = let tempSet = getTempSet instrList
                                 (H.create (List.length instrList)) in
                             let addTempToList t () acc = t::acc in
                             H.fold addTempToList tempSet []
+let mappingForParam paramRegArray i =
+    (* We know that the first 6 params will be mapped to registers, and the
+       rest will be in offsets of 8 bytes above RBP (what was RSP on
+       entrance to the function) *)
+    if i < Array.length paramRegArray then Reg paramRegArray.(i) else
+    MemAddr(RBP, bytesForArg * (i - (Array.length paramRegArray) + 1))
 
 let addParamMappings tmpToAssemLocMap paramRegArray params =
-    List.iteri (fun i -> fun p -> H.add tmpToAssemLocMap p paramRegArray.(i))
+    List.iteri (fun i -> fun p ->
+        H.add tmpToAssemLocMap p (mappingForParam paramRegArray i))
 
 let regAlloc (Tmp2AddrFunDef(fName, params, instrs) : tmp2AddrFunDef) : assemFunDef =
   let paramRegArray = Array.of_list [EDI; ESI; EDX; ECX; R8; R9] in
-  let regList = [EBX; R10; R11; R12; R13; R14; R15] in
+  let allocableRegList = [EBX; R10; R11; R12; R13; R14; R15] in
   (* DO NOT ALLOCATE THE SPILLAGE REGISTER HERE!!! OR REGISTERS USED FOR WONKY *)
-  let regArray = Array.of_list regList in
+  let regArray = Array.of_list allocableRegList in
   let tempList = getTempList instrList in
   let interferenceGraph = LivenessAnalysis.analyzeLiveness instrList tempList params in
   let startVertex = 0 in (* where cardSearch starts from; arbitrary for now *)
@@ -119,7 +154,8 @@ let regAlloc (Tmp2AddrFunDef(fName, params, instrs) : tmp2AddrFunDef) : assemFun
       offsetIncr regArray (H.create 100) in
   let () = addParamMappings tmpToAssemLocMap paramRegArray params in
   let finalOffset = H.fold combineForMaxOffset tmpToAssemLocMap 0 in
-  (pushInstrs) @ [SUBQ(Const finalOffset, Reg RSP)] @
+  (* Move RSP into RBP before we change RSP! *)
+  (pushCalleeInstrs) @ MOVQ(TmpLoc (Reg RSP), Reg RBP)::SUBQ(Const finalOffset, Reg RSP)::[] @
   (List.concat (List.map (translate tmpToAssemLocMap finalOffset paramRegArray) instrList))
 
 (* NEED TO ACTUALLY MOVE ARGS INTO THE CORRESPONDING REGISTERS *)
