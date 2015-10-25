7b5589b23d5f514246d1b3ed3479af18ea9f9f02
Benjamin Plaut <bplaut@unix6.andrew.cmu.edu>
2015-10-24 21:09:18 -0400

    Ok I think array accesses are actually finally done...I can't actually compile it obviously but merlin allowed to me to fix (what should be) all of the type errors

--- rattata/lab4/compiler/front_end/memStuffToInfAddr/memStuffToInfAddr.ml
+++ rattata/lab4/compiler/front_end/memStuffToInfAddr/memStuffToInfAddr.ml
@@ -50,59 +50,35 @@ let getSizeForType = function
    | Pointer _ -> ptrFieldSize
    | Array _ -> ptrFieldSize
    | Struct structTypeName ->
-        try let (_, structSize) = H.find structDefsMap structTypeName in
+        (try let (_, structSize) = H.find structDefsMap structTypeName in
             structSize
         with Not_found -> (let () = print_string("struct " ^ structTypeName
                              ^ "not defined before alloc\n") in
-                           assert(false))
+                           assert(false)))
+   | VOID -> assert(false)
+   | TypedefType _ -> assert(false)
+
+let sharedExprToTypedExpr exprType sharedExpr =
+     match exprType with
+         INT -> TmpIntExpr (TmpIntSharedExpr sharedExpr)
+       | BOOL -> TmpBoolExpr (TmpBoolSharedExpr sharedExpr)
+       | Pointer _ -> TmpPtrExpr (TmpPtrSharedExpr sharedExpr)
+       | _ -> assert(false)
 
 (* we need the type in order to calculate array offsets *)
-let rec handleSharedExpr typee = function
-     TmpInfAddrFunCall (fName, args) -> (TmpInfAddrFunCall(fName, args), [])
+let rec handleSharedExpr exprType = function
+     TmpInfAddrFunCall (fName, args) ->
+        (sharedExprToTypedExpr exprType (TmpInfAddrFunCall(fName, args)), [])
    | TmpInfAddrDeref (ptrExp) ->
-       let (e_result, instrs) = handleMemForExpr ptrExp in
-       (TmpInfAddrDeref e_result, instrs)
-   (* | TmpInfAddrFieldAccess(structPtr, fieldName) -> *)
-   (*     try let (fieldOffsets, _) = H.find structDefsMap structTypeName in *)
-   (*     with Not_found -> (let () = print_string("struct " ^ structTypeName *)
-   (*                                            ^ "not defined before alloc\n") in *)
-   (*                      assert(false)) *)
+       let (TmpPtrExpr e_result, instrs) = handleMemForExpr (TmpPtrExpr ptrExp) in
+       (sharedExprToTypedExpr exprType (TmpInfAddrDeref e_result), instrs)
+   | TmpInfAddrFieldAccess(structTypeName, structPtr, fieldName) -> assert(false)
+       (* try let (fieldOffsets, _) = H.find structDefsMap structTypeName in *)
+       (* with Not_found -> (let () = print_string("struct " ^ structTypeName *)
+       (*                                        ^ "not defined before alloc\n") in *)
+       (*                  assert(false)) *)
    | TmpInfAddrArrayAccess (ptrExp, indexExpr) ->
-       let (ptr_final, ptr_instrs) = handleMemForExpr ptrExp in
-       let (index_final, index_instrs) = handleMemForExpr indexExpr in
-       let elemSize = getSizeForType typee in
-       (* The number of elems is stored at the address ptr_final - 8 *)
-       let numElemsPtr = TmpInfAddrSub64(ptr_final, TmpIntArg (TmpConst 8)) in
-       let numElemsExpr = TmpIntSharedExpr (TmpInfAddDeref numElemsPtr) in
-       let resultTmp = Tmp (Temp.create()) in
-       let errorLabel = GenLabel.create() in
-       let doTheAccessLabel = GenLabel.create() in
-       (* Note that the operand order for cmp has already been reversed!
-          So cmp (a,b) followed by jg will jump is b > a *)
-       let indexLowerCheck = TmpInfAddrCmp32(
-           TmpIntExpr (TmpIntArg (TmpConst 0)), TmpIntExpr index_final)::
-                             TmpInfAddrJump(JL, errorLabel)::[] in
-       let indexUpperCheck = TmpInfAddrCmp32(
-           TmpIntExpr (numElemsExpr, TmpIntExpr index_final))::
-                             TmpInfAddrJump(JGE, errorLabel)::
-                             TmpInfAddrJump(JL, doTheAccessLabel)::[] in
-       let throwError = TmpInfAddrVoidFunCall("raise",
-                              TmpIntExpr (TmpIntArg (TmpConst 12))) in
-       let accessOffsetExpr = TmpInfAddrBinop(MUL, getSizeForType typee,
-                                              numElemsExpr) in
-       let accessPtrExpr = TmpInfAddrAdd64(ptr_final, accessOffsetExpr) in
-       let doTheAccess = TmpInfAddrMov(TmpInfAddrDeref accessPtrExpr,
-                                       resultTmp) in
-      (* Ok now actually put all of the instructions together *)
-      (* The array pointer is evaluated first, I checked *)
-      let allInstrs = ptr_instrs @ index_instrs @ indexLowerCheck
-             @ indexUpperCheck @ TmpInfAddrLabel(errorLabel)::throwError
-             ::TmpInfAddrLabel(doTheAccessLabel)::doTheAccess::[] in
-      (resultTmp, allInstrs)
-
-                             
-
-       
+       handleArrayAccess exprType ptrExp indexExpr
 
 (* returns (e, instrs) pair *)
 and handleMemForExpr = function
@@ -110,8 +86,8 @@ and handleMemForExpr = function
     | TmpPtrExpr (TmpPtrArg arg) -> (TmpPtrExpr (TmpPtrArg arg), [])
     | TmpIntExpr (TmpIntArg arg) -> (TmpIntExpr (TmpIntArg arg), [])
     | TmpIntExpr (TmpInfAddrBinopExpr (op, e1, e2)) ->
-        let (e1_result, instrs1) = handleMemForExpr e1 in
-        let (e2_result, instrs2) = handleMemForExpr e2 in
+        let (TmpIntExpr e1_result, instrs1) = handleMemForExpr (TmpIntExpr e1) in
+        let (TmpIntExpr e2_result, instrs2) = handleMemForExpr (TmpIntExpr e2) in
         (* We have to make sure we're evaluating e1 before e2. So if
            there were any instructions created that involved evaluating
            e2, we need to make sure e1 happens before. We do this
@@ -119,21 +95,77 @@ and handleMemForExpr = function
         let (final_e1, final_instrs1) =
              (match instrs2 with
                  [] -> (e1_result, instrs1)
-               | _ -> (let t = Tmp (Temp.create()) in
-                       (t, instrs1 @ TmpInfAddrMov(e1_result, t)::[]))) in
-        (TmpInfAddrBinop(op, final_e1, e2_result), final_instrs1 @ instrs2)
-    | TmpPtrExpr (TmpAlloc typee) -> TmpPtrExpr (TmpPtrSharedExpr
+               | _ -> (let t = Tmp (Temp.create()) in (TmpIntArg (TmpLoc t),
+                       instrs1 @ TmpInfAddrMov32(TmpIntExpr e1_result, t)::[])))
+            in
+        (TmpIntExpr (TmpInfAddrBinopExpr(op, final_e1, e2_result)),
+         final_instrs1 @ instrs2)
+    | TmpPtrExpr (TmpAlloc typee) -> 
            (* alloc becomes a function call to malloc *) 
-             (TmpInfAddrFunCall ("malloc", getSizeForType typee)))
-    | TmpBoolExpr (TmpBoolSharedExpr e) -> handleSharedExpr e
-    | TmpIntExpr (TmpIntSharedExpr e) -> handleSharedExpr e
-    | TmpPtrExpr (TmpPtrSharedExpr e) -> handleSharedExpr e
-                                    
+             (TmpPtrExpr (TmpPtrSharedExpr (TmpInfAddrFunCall ("malloc",
+              TmpIntExpr (TmpIntArg (TmpConst (getSizeForType typee)))::[]))),
+              [])
+                                         
+    | TmpBoolExpr (TmpBoolSharedExpr e) ->
+         let (e_result, instrs) = handleSharedExpr BOOL e in
+         (TmpBoolExpr (TmpBoolSharedExpr e_result), instrs)
+    | TmpIntExpr (TmpIntSharedExpr e) -> 
+         let (e_result, instrs) = handleSharedExpr INT e in
+         (TmpBoolExpr (TmpBoolSharedExpr e_result), instrs)
+    (* | TmpPtrExpr (TmpPtrSharedExpr e) -> handleSharedExpr Pointer *)
+    | _ -> failwith "not yet implemented"          
+
+  
+and handleArrayAccess elemType ptrExp indexExpr =
+       let (TmpPtrExpr ptr_final, ptr_instrs) =
+           handleMemForExpr (TmpPtrExpr ptrExp) in
+       let (TmpIntExpr index_final, index_instrs) =
+           handleMemForExpr (TmpIntExpr indexExpr) in
+       let elemSize = getSizeForType elemType in
+       (* The number of elems is stored at the address ptr_final - 8 *)
+       let numElemsPtr = TmpInfAddrSub64(ptr_final, TmpIntArg (TmpConst 8)) in
+       let numElemsExpr = TmpIntSharedExpr (TmpInfAddrDeref numElemsPtr) in
+       let resultTmp = Tmp (Temp.create()) in
+       let errorLabel = GenLabel.create() in
+       let doTheAccessLabel = GenLabel.create() in
+       (* Note that the operand order for cmp has already been reversed!
+          So cmp (a,b) followed by jg will jump is b > a *)
+       let indexLowerCheck = TmpInfAddrBoolInstr (TmpInfAddrCmp32(
+           TmpIntExpr (TmpIntArg (TmpConst 0)), TmpIntExpr index_final))::
+           TmpInfAddrJump(JL, errorLabel)::[] in
+       let indexUpperCheck = TmpInfAddrBoolInstr (TmpInfAddrCmp32(
+           TmpIntExpr numElemsExpr, TmpIntExpr index_final))::
+                             TmpInfAddrJump(JGE, errorLabel)::
+                             TmpInfAddrJump(JL, doTheAccessLabel)::[] in
+       let throwError = TmpInfAddrVoidFunCall("raise",
+                              TmpIntExpr (TmpIntArg (TmpConst 12))::[]) in
+       let accessOffsetExpr = TmpInfAddrBinopExpr(MUL,
+                          TmpIntArg (TmpConst (getSizeForType elemType)),
+                                              numElemsExpr) in
+       let accessPtrExpr = TmpInfAddrAdd64(ptr_final, accessOffsetExpr) in
+       let doTheAccess = (match elemType with
+                    BOOL -> TmpInfAddrMov32(TmpBoolExpr (TmpBoolSharedExpr
+                               (TmpInfAddrDeref accessPtrExpr)), resultTmp)
+                  | INT -> TmpInfAddrMov32(TmpIntExpr (TmpIntSharedExpr
+                               (TmpInfAddrDeref accessPtrExpr)), resultTmp)
+                  | Pointer _ -> TmpInfAddrMov64(TmpPtrSharedExpr
+                               (TmpInfAddrDeref accessPtrExpr), resultTmp)
+                  | _ -> assert(false)) in
+      (* Ok now actually put all of the instructions together *)
+      (* The array pointer is evaluated first, I checked *)
+      let allInstrs = ptr_instrs @ index_instrs @ indexLowerCheck
+             @ indexUpperCheck @ TmpInfAddrLabel(errorLabel)::throwError
+             ::TmpInfAddrLabel(doTheAccessLabel)::doTheAccess::[] in
+      let resultTmpExpr = (match elemType with
+                         INT -> TmpIntExpr (TmpIntArg (TmpLoc resultTmp))
+                       | BOOL -> TmpBoolExpr (TmpBoolArg (TmpLoc resultTmp))
+                       | Pointer _ -> TmpPtrExpr (TmpPtrArg (TmpLoc resultTmp))
+                       | _ -> assert(false))
+      in (resultTmpExpr, allInstrs)
 
 let handleMemForInstr = function
       TmpInfAddrJump j -> TmpInfAddrJump j::[]
     | TmpInfAddrLabel lbl -> TmpInfAddrLabel lbl::[]
-    | TmpInfAddrMov32 (src, dest) ->
       
 
 let handleMemForFunDef (fName, tmpParams, instrs) =
