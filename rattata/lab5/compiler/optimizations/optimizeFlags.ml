caa62ffef15f3c291ae8f0c4ec9b10de2d1e4daa
Benjamin Plaut <bplaut@unix1.andrew.cmu.edu>
2015-11-13 14:46:38 -0500

    whoops forgot to add the actual optimizeFlags.ml file

---
+++ rattata/lab5/compiler/optimizations/optimizeFlags.ml
@@ -0,0 +1,8 @@
+(* Contains various flags for optimization. These are refs so that they can be
+   set by top.ml and accesses throughout the compiler without having to pass
+   them as arguments everything *)
+
+(* Defaults are the behavior of -01 *)
+let safeMode = ref true (* default: do null ptr, array bounds, shift checks *)
+let doRegAlloc = ref true (* default: do register allocation *)
+let removeDeadCode = ref false (* don't remove dead code *)
